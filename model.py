088e3c572c5194fd83bf47ee922139e090017fe0
Sofiene Bourghes <sofiene.bourghes@epita.fr>
2020-11-07 16:04:22 +0100

    update model with collab experiment code

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
+import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 class Net(nn.Module):
     def __init__(self):
@@ -9,13 +11,17 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 3)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
+        #print('Shape before flatten:', x.shape)
+        x = x.view(x.size(0), -1)
+        #print('Shape after flatten:', x.shape)
+        #print('Shape fully connected:', self.fc1.shape)
+        self.linear_input_size = x.shape[1]
+        x = F.relu(nn.Linear(self.linear_input_size, 120)(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
