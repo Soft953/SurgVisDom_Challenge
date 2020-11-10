4ad371bfe641b5807b31bc35e3bb2ceb77e1bc5e
Sofiene Bourghes <sofiene.bourghes@epita.fr>
2020-11-10 12:34:46 +0100

    add test set loader

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
 import glob
 import numpy as np
+import pandas as pd
 import cv2
 from PIL import Image
+from torch import stack
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 
@@ -40,7 +43,6 @@ class SurgVisDataset(Dataset):
             extracted_imgs_folder = next(os.walk(class_dir))[1]
             for extracted_folder in extracted_imgs_folder:
                 files_path = [os.path.join(class_dir, extracted_folder, x) for x in os.listdir(os.path.join(class_dir, extracted_folder))]
-                #import pdb; pdb.set_trace()
                 X.append(files_path)
                 y.append(value)
         return X, y
@@ -73,13 +75,78 @@ class SurgVisDataset(Dataset):
         self.length = sum
         return self.length
 
+
+class SurgVisTestset:
+    """
+        Simple Custom Dataset because test set is too big to be saved and loaded frame by frame
+    """
+    def __init__(self, path, transform=None, verbose=True):
+        self.path = Path(path)
+
+        if transform is None:
+            self.transform = transforms.Compose([transforms.ToTensor()])
+        else:
+            self.transform = transform
+
+        self.metadata = pd.read_csv(self.path.joinpath('testing_set_video_metadata.csv'))
+        self.X = [[] for i in range(len(self.metadata))]
+
+        self.verbose = verbose
+
+        if self.verbose:
+            print(self.metadata.head())
+            print(len(self.X))
+
+    def _extract_frames_from_videos(self, vid_path : Path, frame_batch):
+        cap = cv2.VideoCapture(str(vid_path))
+
+        frames = []
+        for idx in frame_batch:
+            # set frame position
+            cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
+            ret, frame = cap.read()
+            frames.append(frame)
+        
+        return frames
+
+    # Path('C:\\Users\\gbour\\Desktop\\sysvision\\test')
+    def _load_frames(self, vid_index, frame_batch):
+        vid_path = self.path.joinpath(self.metadata.vid_name.iloc[vid_index] + '.mp4')
+        return self._extract_frames_from_videos(vid_path, frame_batch)
+
+    def _preprocess_frame(self, img):
+        #Apply transformation (Conversion, Data augmentation, ...)
+        return self.transform(img)
+
+    def get_batches(self, vid_index, indexes):
+        frames = self._load_frames(vid_index, indexes)
+        list_of_tensors = []
+        for frame in frames:
+            #print(type(frame), frame.shape, frame.size())
+            frame_as_tensor = self.transform(frame)
+            list_of_tensors.append(frame_as_tensor)
+        return stack(list_of_tensors)
+
+CROP_SIZE = (420, 630)
+INPUT_SHAPE = (256, 256)
+
+train_transform = transforms.Compose([transforms.ToPILImage(),
+                                        transforms.CenterCrop(CROP_SIZE),
+                                        transforms.Resize(INPUT_SHAPE),
+                                        transforms.ToTensor()
+                                    ])
+
+test_set = SurgVisTestset(path=Path('C:\\Users\\gbour\\Desktop\\sysvision\\test'), transform=train_transform, verbose=True)
+batches = test_set.get_batches(0, list(range(32)))
+print(batches.shape)
+
+"""
 import matplotlib.pyplot as plt
 
 PATH = 'C:\\Users\\gbour\\Desktop\\sysvision\\train_1'
 PATH_PORCINE_1 = os.path.join(PATH, 'Porcine')
 
-CROP_SIZE = (420, 630)
-INPUT_SHAPE = (256, 256)
+
 
 train_transform = transforms.Compose([transforms.CenterCrop(CROP_SIZE),
                                         transforms.Resize(INPUT_SHAPE),
@@ -89,6 +156,11 @@ dataset = SurgVisDataset(PATH_PORCINE_1, transform=train_transform, verbose=True
 #img, y = dataset.__getitem__(0)
 trans = transforms.ToPILImage()
 trans1 = transforms.ToTensor()
+
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+images, labels = next(iter(dataloader))
+
+
 
 n = len(dataset)
 n_train = int(n * 0.9)
@@ -108,6 +180,7 @@ import pdb; pdb.set_trace()
 
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 images, labels = next(iter(dataloader))
+"""
 """
 plt.figure()
 plt.imshow(trans(images[0]))
--- extract_imgs_from_vids.py
+++ extract_imgs_from_vids.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import cv2
 
 def _extract_imgs_from_videos(vid_path):
@@ -22,9 +23,8 @@ def _extract_imgs_from_videos(vid_path):
         i += 1
         #cv2.resize(frame[60:490, 165:795], (256, 256))))
 
-def _load_data_per_classes():
-    PATH = 'C:\\Users\\gbour\\Desktop\\sysvision\\train_1'
-    PATH_PORCINE_1 = os.path.join(PATH, 'Porcine')
+def _load_data_per_classes(path):
+    PATH_PORCINE_1 = os.path.join(path, 'Porcine')
     path = PATH_PORCINE_1
     for key, value in {'Dissection': 0, 'Knot_Tying': 1, 'Needle_Driving': 2}.items():
         class_dir = os.path.join(path, key)
@@ -34,5 +34,15 @@ def _load_data_per_classes():
         for f in files_path:
             _extract_imgs_from_videos(f)
 
-_load_data_per_classes()
+if __name__ == '__main__':
+    # PATH = 'C:\\Users\\gbour\\Desktop\\sysvision\\train_1'
+    PATH = Path('C:\\Users\\gbour\\Desktop\\sysvision\\test')
+    if PATH.name == "test":
+        for vid_path in PATH.iterdir():
+            #vid_name = vid_path.name.split('.')[0]
+            #PATH.joinpath(vid_name).mkdir()
+            #print(PATH.joinpath(vid_name))
+            _extract_imgs_from_videos(str(vid_path))
+    else:
+        _load_data_per_classes(PATH)
 
