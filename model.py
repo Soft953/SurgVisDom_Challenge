b096e13b7395e0bb1153cb135f0ceae3b5fba589
Sofiene Bourghes <sofiene.bourghes@epita.fr>
2020-11-11 19:28:27 +0100

    Update model architecture, training loop, add wandb support, learning rate scheduler, random_split with seed

--- model.py
+++ model.py
@@ -6,23 +6,25 @@ import torch
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+
         self.conv1 = nn.Conv2d(3, 6, 5)
+        self.bn1 = nn.BatchNorm2d(6)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 61 * 61, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 3)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, 5)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.drop = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(25088, 128) #nn.Linear(32 * 61 * 61, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 3)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        # print('Shape before flatten:', x.shape)
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
         x = x.view(x.size(0), -1)
-        # print('Shape after flatten:', x.shape)
-        #print('Shape fully connected:', self.fc1.shape)
-        # self.linear_input_size = x.shape[1]
-        #x = F.relu(nn.Linear(self.linear_input_size, 120)(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.drop(F.relu(self.fc1(x)))
+        x = self.drop(F.relu(self.fc2(x)))
         x = self.fc3(x)
         return x
--- train.py
+++ train.py
@@ -1,100 +1,163 @@
-import torch.optim as optim
+from pathlib import Path
+import time
+
+import wandb
+
+import torch
+import torch.nn as nn
+from torch.optim import SGD
+from torch.optim.lr_scheduler import MultiStepLR
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+
+from data_loader import SurgVisDataset
+from model import Net
 
 
-def train(trainloader, val_loader, net, dev):
+def train(trainloader, val_loader, model, dev, config):
     #define optimizer and loss function
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = SGD(model.parameters(), lr=config.lr, momentum=config.momentum)
+    epoch_milestones = [int(config.epochs/2), int(config.epochs/2 + config.epochs/4)]
+    scheduler = MultiStepLR(optimizer, milestones=epoch_milestones, gamma=0.1)
 
+    train_loss_history = []
+    val_loss_history = []
 
-    # train/val epoch_loss_history
-    train_epoch_loss_history = []
-    val_epoch_loss_history = []
+    # training/validation loop
+    for epoch in range(config.epochs):
+        start = time.time()
+        training_loss = 0.0
+        validation_loss = 0.0
+        correct_training = 0
+        correct_validation = 0
 
-    trainloader_iter = iter(trainloader)
-    val_loader_iter = iter(val_loader)
+        # Set the model to training mode
+        model.train()
+        
+        #Training step loop
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            # Tensor to cpu or gpu
+            inputs, labels = inputs.to(dev), labels.to(dev)
 
-    #training loop
-    for epoch in range(EPOCHS):  # loop over the dataset multiple times
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # batches_per_epoch = len(dataset) // BATCH_SIZE # useless
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
 
-        for phase in ['train', 'val']:
-          start = time.time()
-          running_loss = 0.0
+            # Compute accuracy
+            pred = outputs.max(1, keepdim=True)[1]
+            correct_per_batches = pred.eq(labels.view_as(pred)).sum().item()
+            correct_training += correct_per_batches
 
-          if phase == 'train':
-            net.train(True) # Set model to training mode
-          else:
-            net.train(False) # Set model to evaluate mode
+            loss.backward()
+            optimizer.step()
 
-          print('Phase:', phase)
+            # print statistics
+            training_loss += loss.item()
 
-          loader = trainloader if phase == 'train' else val_loader
-  
-          for i, data in enumerate(loader, 0):
-              # get the inputs; data is a list of [inputs, labels]
-              # delete after
-              if phase == 'train':
-                inputs, labels = data #next(trainloader_iter)
+            # Log acc and loss per batches
+            # wandb.log({"Train accuracy per batches": correct_per_batches / len(labels), "Train loss per batches": loss.item()})
+
+        mean_training_loss = training_loss / (i+1)
+        train_loss_history.append(mean_training_loss)
+        end = time.time()
+        print('Training step: [%d, %5d] loss: %.3f' % (epoch + 1, config.epochs, mean_training_loss), end="")
+        print("| time elapsed (in sec):", end - start)
+        # wandb.log({"Train accuracy per epoch": correct / len(trainloader.dataset), "Train loss per epoch": mean_training_loss})
+
+        # Validation step
+        model.eval()
+        with torch.no_grad():
+            for i, data in enumerate(val_loader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                # Tensor to cpu or gpu
                 inputs, labels = inputs.to(dev), labels.to(dev)
-              else:
-                inputs, labels = data #next(val_loader_iter)
-                inputs, labels = inputs.to(dev), labels.to(dev)
 
-              #print('Input shape:', inputs.shape)
-              #print('Epoch', epoch, ': [%d, %d]' % (i, batches_per_epoch))
-              # zero the parameter gradients
-              optimizer.zero_grad()
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-              # forward + backward + optimize
-              outputs = net(inputs)
-              loss = criterion(outputs, labels)
-              if phase == 'train':
-                loss.backward()
-                optimizer.step()
+                # forward + backward + optimize
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
 
-              # print statistics
-              running_loss += loss.item()
+                # Compute accuracy
+                pred = outputs.max(1, keepdim=True)[1]
+                correct_per_batches = pred.eq(labels.view_as(pred)).sum().item()
+                correct_validation += correct_per_batches
 
-          mean_epoch_loss = running_loss / (i+1)
-          if phase == 'train':
-            train_epoch_loss_history.append(mean_epoch_loss)
-          else:
-            val_epoch_loss_history.append(mean_epoch_loss)
-          print('Phase %s: [%d, %5d] loss: %.3f' %
-              (phase, epoch + 1, i + 1, mean_epoch_loss), end="")
-          end = time.time()
-          print(" -- time elapsed (in sec):", end - start)
+                # print statistics
+                validation_loss += loss.item()
+
+                # Log acc and loss per batches
+                # wandb.log({"Val accuracy per batches": correct_per_batches / len(labels), "Val loss per batches": loss.item()})
+
+            mean_validation_loss = validation_loss / (i+1)
+            if len(val_loss_history) and mean_validation_loss < np.min(val_loss_history):
+                # Save model to wandb
+                filename = "model_epoch_%d_loss_%.4f.h5" % (epoch + 1, mean_validation_loss)
+                torch.save(model.state_dict(), filename)
+                wandb.save(filename)
+            val_loss_history.append(mean_validation_loss)
+            end = time.time()
+            print('Val step: [%d, %5d] loss: %.3f' % (epoch + 1, config.epochs, mean_validation_loss), end="")
+            print("| time elapsed (in sec):", end - start)
+            # Log everything once
+            wandb.log({"Train accuracy per epoch": correct_training / len(trainloader.dataset),
+                       "Train loss per epoch": mean_training_loss,
+                       "Val accuracy per epoch": correct_validation / len(val_loader.dataset),
+                       "Val loss per epoch": mean_validation_loss})
 
     print('Finished Training')
-    return (train_epoch_loss_history, val_epoch_loss_history)
+    return (train_loss_history, val_loss_history)
+
 
 
 if __name__ == "__main__":
-    BATCH_SIZE = 32
-    EPOCHS = 20
+    wandb.init(project="surgvisdom")
 
-    train_transform = transforms.Compose([transforms.CenterCrop(CROP_SIZE),
-                                            transforms.Resize((256, 256)),
+    PATH = Path('train_1')
+    PATH_PORCINE_1 = PATH.joinpath('Porcine')
+
+    # WandB – Config is a variable that holds and saves hyperparameters and inputs
+    config = wandb.config          # Initialize config
+    config.batch_size = 128        # input batch size for training (default: 64)
+    # config.test_batch_size = 10    # input batch size for testing (default: 1000)
+    config.epochs = 50             # number of epochs to train (default: 10)
+    config.lr = 0.1               # learning rate (default: 0.01)
+    config.momentum = 0.1          # SGD momentum (default: 0.5) 
+    config.no_cuda = False         # disables CUDA training
+    config.seed = 42               # random seed (default: 42)
+    # config.log_interval = 10     # how many batches to wait before logging training status
+    config.crop_size = (420, 630)
+    config.resize_shape = (256, 256)
+    config.dataset_path = PATH_PORCINE_1
+
+    train_transform = transforms.Compose([transforms.CenterCrop(config.crop_size),
+                                            transforms.Resize(config.resize_shape),
                                             transforms.ToTensor()])
 
-    full_dataset = SurgVisDataset(PATH_PORCINE_1, transform=train_transform, verbose=False)
+    dataset = SurgVisDataset(config.dataset_path, transform=train_transform, verbose=False)
 
     n = len(dataset)
-    n_train = int(n * 0.90)
+    n_train = int(n * 0.80)
     n_val = int(n * 0.1)
+    n_dev_test = int(n * 0.1)
     print("dataset len:", n)
 
-
-    train_set, val_set, test_set = random_split(full_dataset, (n_train, n_val, 0))
+    # Always split using the same seed
+    torch.manual_seed(config.seed)
+    train_set, val_set, dev_test_set = random_split(dataset, (n_train, n_val, n_dev_test))
+    torch.manual_seed(torch.initial_seed())
 
     print("Train set:", len(train_set))
     print("Val set:", len(val_set))
-    print("Test set", len(test_set))
-
-    dataloader_train = DataLoader(train_set, batch_size=32, shuffle=True)
-    dataloader_val = DataLoader(val_set, batch_size=32, shuffle=False)
+    print("Test set", len(dev_test_set))
 
     model = Net()
 
@@ -102,4 +165,10 @@ if __name__ == "__main__":
     print('dev:', dev)
     model.to(dev)
 
-    train_history, val_history = train(dataloader_train, dataloader_val, model, dev)
+    # Log metrics with wandb
+    wandb.watch(model)
+
+    dataloader_train = DataLoader(train_set, batch_size=config.batch_size, shuffle=True, num_workers=8)
+    dataloader_val = DataLoader(val_set, batch_size=config.batch_size, shuffle=False, num_workers=2)
+
+    train_history, val_history = train(dataloader_train, dataloader_val, model, dev, config)
